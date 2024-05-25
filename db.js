7a1323b3134798787a5f7227a2b193f9b916d587
manojbhatta500 <manojbhatta500@gmail.com>
2024-05-25 15:55:13 +0545

    fix check mongo problem

--- db.js
+++ db.js
@@ -5,13 +5,22 @@ require('dotenv').config();
 // const mongoUrl= "mongodb://127.0.0.1:27017/";
 
 // mongodb atlas setup
+console.log('render data base connection not wroking checkpoint 1');
+
 
 const mongoUrl= process.env.MONGOURL;
+
+console.log('render data base connection not wroking checkpoint 2');
 
 
 mongoose.connect(mongoUrl,{});
 
+console.log('render data base connection not wroking checkpoint 3');
+
+
 const db = mongoose.connection;
+console.log('render data base connection not wroking checkpoint 4');
+
 
 
 db.on('connected',()=>{
@@ -31,6 +40,9 @@ db.on('reconnected', () => {
     console.log('Mongoose reconnected');
 });
 
+
+
+console.log('render data base connection not wroking checkpoint 5');
 
 
 module.exports = db;
